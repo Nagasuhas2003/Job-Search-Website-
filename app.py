a2ae39c81d48ec4188394d2b9016353c6fde4f2c
Nagasuhas S <124366703+Nagasuhas2003@users.noreply.github.com>
2025-04-23 03:00:59 +0530

    Added Dynamic data and api endpoint

--- app.py
+++ app.py
@@ -3,10 +3,38 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+JOBS =  [
+    {
+        'id':1,
+        'title':'Data Scientist',
+        'location':'Bangalore',
+        'salary':'Rs.120000'
+    },
+    {
+        'id':2.,
+        'title':'Data analyst',
+        'location':'Bangalore',
+        'salary':'Rs.123040'
+    },
+    {
+        'id':3,
+        'title':'Data Engineer',
+        'location':'Bangalore',
+        'salary':'500000'
+    },
+    {
+        'id':4,
+        'title':'Big Data',
+        'location':'Bangalore',
+        'salary':'Rs.456787'
+    }
+]
 @app.route('/')
 def hello_world():
-    #return 'Hello, World! this is naga'
-    return render_template('home.html')
+#return 'Hello, World! this is naga'
+#without using bootstap    return render_template('home.html')
+#with using bootsrap
+    return render_template('home2.html',jobs=JOBS)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
